33ccfa934898229a258dbf2475fe983d622e7448
ADESSO/kalmykov <oleksandr.kalmykov@adesso.it>
2025-05-28 23:20:33 +0200

    test commit

--- easyshop-cart-service/src/main/java/com/easyshop/cart_service/Application.java
+++ easyshop-cart-service/src/main/java/com/easyshop/cart_service/Application.java
@@ -3,6 +3,7 @@ package com.easyshop.cart_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// todo
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
